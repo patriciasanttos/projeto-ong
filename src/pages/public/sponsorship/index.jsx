20c9b6631f4189e124950a7bf04177de10a83b51
Patricia Santos <patriciasanttos.dev@gmail.com>
2026-04-04 22:20:57 -0300

    Filter widths adjusted on mobile.

--- src/pages/public/sponsorship/index.jsx
+++ src/pages/public/sponsorship/index.jsx
@@ -8,8 +8,10 @@ import Menu from "../../../components/menu/Menu";
 import CardAnimal from "../../../components/cardAnimal/CardAnimal";
 import ModalLPSponsorship from "../../../components/modal/modalLPSponsorship";
 import imageDog1 from "../../../assets/images/landing_page/dog1.svg";
+import imageCat from "../../../assets/images/cat-image.jpg";
 import ContentHero from "../../../components/contentHero/ContentHero";
 import LoadingPaw from "../../../components/loadingPaw";
+import FilterSidebar from "../../../components/filterSideBar/FilterSideBar";
 import { getAllAnimals } from "../../../services/api/animals";
 import Pagination from "../../../components/pagination";
 
@@ -18,22 +20,53 @@ function Sponsorship() {
   const [selectedAnimal, setSelectedAnimal] = useState(null);
   const [animals, setAnimals] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [filteredAnimals, setFilteredAnimals] = useState([]);
   const [paginatedAnimals, setPaginatedAnimals] = useState([]);
+  const [filters, setFilters] = useState({
+    species: "",
+    gender: "",
+    age: "",
+    size: "",
+  });
 
   useEffect(() => {
     const getAnimals = async () => {
       const data = await getAllAnimals();
-      const animalsWithImage = data.map((animal) => ({
-        ...animal,
-        image: imageDog1,
-      }));
 
+      const formattedAnimals = data.map((animal) => {
+        const isCat = animal.species === "felina";
+        const defaultImage = isCat ? imageCat : imageDog1;
+
+        return {
+          ...animal,
+          name: animal?.name.charAt(0).toUpperCase() + animal?.name.slice(1).toLowerCase(),
+          race: animal?.race,
+          image: animal?.image
+            ? `data:image/jpg;base64,${animal.image}`
+            : defaultImage,
+        };
+      });
+
+      setAnimals(formattedAnimals);
+      setFilteredAnimals(formattedAnimals);
       setLoading(false);
-      setAnimals(animalsWithImage);
     };
 
     getAnimals();
   }, []);
+
+  const getFilteredAnimals = (items) => {
+    return items.filter((animal) => {
+      return Object.entries(filters).every(([key, value]) => {
+        if (value === "") return true;
+        return String(animal[key]).toLowerCase() === value.toLowerCase();
+      });
+    });
+  };
+
+  useEffect(() => {
+    setFilteredAnimals(getFilteredAnimals(animals));
+  }, [filters]);
 
   const onClickCardAnimal = (animal) => {
     setIsModalOpen(true);
@@ -54,39 +87,32 @@ function Sponsorship() {
         <LoadingPaw />
       ) : (
         <div className="contentContainer">
+          <FilterSidebar
+            filtersState={filters}
+            setFiltersState={setFilters}
+          />
+
           <p className="text">Conheça alguns de nossos animais</p>
 
-          <div className="photoGalleryCenter">
-            <div className="photoGallery">
-              <div className="cardContainer">
-                {paginatedAnimals.map((animal) => (
-                  <CardAnimal
-                    onClickButton={onClickCardAnimal}
-                    animal={animal}
-                    key={animal.id}
-                    image={animal.image}
-                    name={
-                      animal.name.charAt(0).toUpperCase() +
-                      animal.name.slice(1).toLowerCase()
-                    }
-                    gender={
-                      ["m", "M"].includes(animal.gender) ? "Macho" : "Fêmea"
-                    }
-                    race={
-                      animal.race === "srd"
-                        ? "Sem raça definida"
-                        : `${animal.race.charAt(0).toUpperCase()}${animal.race
-                            .slice(1)
-                            .toLowerCase()}`
-                    }
-                    age={animal.age}
-                  />
-                ))}
-              </div>
-            </div>
+          <div className="cardContainer">
+            {paginatedAnimals.map((animal) => (
+              <CardAnimal
+                onClickButton={onClickCardAnimal}
+                animal={animal}
+                key={animal.id}
+                image={animal.image}
+                name={animal.name}
+                gender={
+                  ["m", "macho"].includes(animal.gender.toLowerCase()) ? "Macho" : "Fêmea"
+                }
+                race={animal.race}
+                age={animal.age}
+              />
+            ))}
           </div>
+
           <Pagination
-            listItems={animals}
+            listItems={filteredAnimals}
             onPaginate={(items) => setPaginatedAnimals(items)}
           />
         </div>
