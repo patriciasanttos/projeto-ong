d71679d3df99b522ae1ea64e24389981b9192761
Patricia Santos <patriciasanttos.dev@gmail.com>
2026-04-04 23:29:34 -0300

    Adjusted and responsive address alignment

--- src/pages/public/contact/index.jsx
+++ src/pages/public/contact/index.jsx
@@ -43,6 +43,21 @@ function Contact() {
                 CEP 00000-000, Cidade - UF
               </p>
             </div>
+
+            <div className="map-placeholder">
+              <div className="map-placeholder-inner">
+                <svg className="map-pin-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z" fill="#26A59E"/>
+                </svg>
+                <div className="map-streets">
+                  <div className="map-street-h map-street-1"></div>
+                  <div className="map-street-h map-street-2"></div>
+                  <div className="map-street-v map-street-3"></div>
+                  <div className="map-street-v map-street-4"></div>
+                </div>
+                <span className="map-label">Google Maps</span>
+              </div>
+            </div>
           </section>
 
           <section className="contacts">
